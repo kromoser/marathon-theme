0c3157ca7d54dd7ba1d2d337d530273127a81103
kromoser <kevinromoser@gmail.com>
2019-03-23 13:18:19 -0400

    Add Typekit fonts, style animated hero text (webkit only)

--- resources/views/partials/hero-section.blade.php
+++ resources/views/partials/hero-section.blade.php
@@ -1,16 +1,16 @@
 <section class="hero-section columns">
-  <div class="column is-7">
-    <h1 class="has-text-white	">Miles to Go</h1>
-    <h2 class="has-text-white	">Running the 2019 NYC Marathon to raise money for Housing Works</h2>
-    <p class="has-text-warning stat-header">The Race</p>
-    <h3 class="has-text-white">26.2 <span class="has-text-grey">miles</span></h3>
-    <p class="has-text-warning stat-header">The Goal</p>
-    <h3 class="has-text-white">$100<span class="has-text-grey">/$2,500</span></h3>
-    <p class="has-text-warning stat-header">The Training</p>
-    <h3 class="has-text-white">285 <span class="has-text-grey">miles logged</span></h3>
+  <div class="column is-8">
+    <h1 class="hero-title">Miles to&nbsp;Go</h1>
+    <h2 class="hero-subtitle">Running the 2019 NYC Marathon to raise money for Housing Works</h2>
+    <p class="stat-header">The Race</p>
+    <h3 class="stat-highlight">26.2 <span class="stat-secondary">miles</span></h3>
+    <p class="stat-header">The Goal</p>
+    <h3 class="stat-highlight">$100<span class="stat-secondary">/$2,500</span></h3>
+    <p class="stat-header">The Training</p>
+    <h3 class="stat-highlight">285 <span class="stat-secondary">miles logged</span></h3>
   </div>
 
-  <div class="column is-5">
+  <div class="column is-4">
     Map TK
   </div>
 </section>
